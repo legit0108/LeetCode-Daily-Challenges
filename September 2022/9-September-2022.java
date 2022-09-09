a73caf11c18132f65b7ff4d56719280d62afed54
legit0108 <legitgamer2345@gmail.com>
2022-09-09 15:53:33 +0530

    The Number of Weak Characters in the Game

---
+++ September 2022/9-September-2022.java
@@ -0,0 +1,19 @@
+// TC : O(len*log(len))
+// SC : O(len)
+
+class Solution {
+    public int numberOfWeakCharacters(int[][] properties) {
+        int len = properties.length;
+        Arrays.sort(properties, (property1, property2)->(property1[0] == property2[0])?
+                                Integer.compare(property1[1], property2[1]) : Integer.compare(property2[0], property1[0]));
+        int maxDefense = properties[0][1];
+        int weakCharacters = 0;
+        
+        for(int idx = 1;idx<len;idx++){
+            if(properties[idx][1] < maxDefense) weakCharacters++;
+            else maxDefense = properties[idx][1];
+        }
+        
+        return weakCharacters;
+    }
+}
