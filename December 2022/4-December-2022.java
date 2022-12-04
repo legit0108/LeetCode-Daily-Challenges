d0de8902ff39b348eb0565bc64533934c6d07371
legit0108 <legitgamer2345@gmail.com>
2022-12-04 08:40:27 +0530

    Minimum Average Difference

---
+++ December 2022/4-December-2022.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int minimumAverageDifference(int[] nums) {
+        int len = nums.length;
+        long sum = 0;
+        
+        for(int idx=0; idx<len; idx++){
+            long val = nums[idx];
+            sum+=val;
+        }
+        
+        long prefixSum = 0;
+        long minAvgDiff = Long.MAX_VALUE;
+        int index = -1;
+        
+        for(int idx=0; idx<len; idx++){
+            long val = nums[idx];
+            prefixSum+=val;
+            long suffixSum = sum-prefixSum;
+            
+            long prefixLen = idx+1;
+            long suffixLen = len-idx-1;
+            
+            long prefixAvg = (prefixLen==0) ? 0 : prefixSum/prefixLen;
+            long suffixAvg = (suffixLen==0) ? 0 : suffixSum/suffixLen;
+            
+            long currAvgDiff = Math.abs(prefixAvg-suffixAvg);
+            if(currAvgDiff < minAvgDiff){
+                minAvgDiff = currAvgDiff;
+                index = idx;
+            }
+        }
+        
+        return index;
+    }
+}class Solution {
+    public int minimumAverageDifference(int[] nums) {
+        int len = nums.length;
+        long sum = 0;
+        
+        for(int idx=0; idx<len; idx++){
+            long val = nums[idx];
+            sum+=val;
+        }
+        
+        long prefixSum = 0;
+        long minAvgDiff = Long.MAX_VALUE;
+        int index = -1;
+        
+        for(int idx=0; idx<len; idx++){
+            long val = nums[idx];
+            prefixSum+=val;
+            long suffixSum = sum-prefixSum;
+            
+            long prefixLen = idx+1;
+            long suffixLen = len-idx-1;
+            
+            long prefixAvg = (prefixLen==0) ? 0 : prefixSum/prefixLen;
+            long suffixAvg = (suffixLen==0) ? 0 : suffixSum/suffixLen;
+            
+            long currAvgDiff = Math.abs(prefixAvg-suffixAvg);
+            if(currAvgDiff < minAvgDiff){
+                minAvgDiff = currAvgDiff;
+                index = idx;
+            }
+        }
+        
+        return index;
+    }
+}
