2363f025b5b0a5120c8570ea49ef241fdb43753b
legit0108 <legitgamer2345@gmail.com>
2022-12-05 14:50:33 +0530

    Middle of the Linked List

---
+++ December 2022/5-December-2022.java
@@ -0,0 +1,18 @@
+// TC : O(length(list))
+// SC : O(1)
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head==null || head.next==null) return head;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+}
