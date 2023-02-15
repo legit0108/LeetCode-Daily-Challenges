43e2a63ae7113b419c925378505725175477bdfa
legit0108 <legitgamer2345@gmail.com>
2023-02-15 13:08:36 +0530

    989. Add to Array-Form of Integer

---
+++ February 2023/15-February-2023.java
@@ -0,0 +1,27 @@
+// Simple Math
+
+// TC: O(max(len, logk))
+// SC: O(max(len, logk))
+
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        LinkedList<Integer> array_form = new LinkedList();
+        int len = num.length;
+        int idx = len-1;
+        int carry = 0;
+        
+        while(idx>=0 || k>0 || carry>0){
+            int sum = ((idx>=0)?num[idx]:0) + k%10 + carry;
+            
+            int digit = sum%10;
+            carry = sum/10;
+            
+            array_form.addFirst(digit);
+            
+            idx--;
+            k = k/10;
+        }
+        
+        return array_form;
+    }
+}
