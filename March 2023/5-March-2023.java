305e9d6ccaf57d504e80e55da221924e4913d31f
legit0108 <legitgamer2345@gmail.com>
2023-03-05 12:46:14 +0530

    1345. Jump Game IV

--- March 2023/5-March-2023.java
+++ March 2023/5-March-2023.java
@@ -1,4 +1,4 @@
-// BFS
+// Solution-1: BFS
 
 // TC: O(len)
 // SC: O(len)
@@ -46,6 +46,73 @@ class Solution {
                 }
             }
             
+            minSteps++;
+        }
+        
+        return -1;
+    }
+}
+
+
+/*
+ Solution-2: Bidirectional BFS
+
+ In the later part of our original BFS method, the layer may be long and takes a long time to compute the next layer. 
+ In this situation, we can compute the layer from the end, which may be short and takes less time.
+
+ TC: O(len)
+ SC: O(len)
+*/ 
+
+class Solution {
+    public int minJumps(int[] arr) {
+        int len = arr.length;
+        HashMap<Integer, List<Integer>> map = new HashMap();
+        
+        for(int idx=0; idx<len; idx++){
+            int val = arr[idx];
+            if(!map.containsKey(val)) map.put(val, new ArrayList());
+            map.get(val).add(idx);
+        }
+        
+        HashSet<Integer> queue1 = new HashSet();
+        HashSet<Integer> queue2 = new HashSet();
+        HashSet<Integer> visited = new HashSet();
+        queue1.add(0);
+        queue2.add(len-1);
+        int minSteps = 0;
+        
+        while(queue1.size()>0){
+            if(queue1.size()>queue2.size()){ // search from the side with fewer nodes
+                HashSet<Integer> temp = queue1;
+                queue1 = queue2;
+                queue2 = temp;
+            }
+            
+            HashSet<Integer> queue = new HashSet();
+            
+            for(int idx: queue1){
+                if(visited.contains(idx)) continue;
+                if(queue2.contains(idx)) return minSteps; // contains in O(1) if queue is hashset
+                
+                visited.add(idx);
+                
+                if(idx+1<len && !visited.contains(idx+1)) queue.add(idx+1); // op-1
+                if(idx-1>=0 && !visited.contains(idx-1)) queue.add(idx-1); // op-2
+                
+                int val = arr[idx];
+                if(map.containsKey(val)){
+                    List<Integer> list = map.get(val);
+                    
+                    for(int nextIdx: list){
+                        if(!visited.contains(nextIdx)) queue.add(nextIdx);
+                    }
+                    
+                    map.remove(val); // Only need to perform op-3 once for a value to get min moves
+                }
+            }
+            
+            queue1 = queue;
             minSteps++;
         }
         
