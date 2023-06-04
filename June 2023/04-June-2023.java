a3bff5224b36cfdfa4656d790da8ca4e08aedbb3
legit0108 <legitgamer2345@gmail.com>
2023-06-05 01:27:08 +0530

    547. Number of Provinces

---
+++ June 2023/04-June-2023.java
@@ -0,0 +1,28 @@
+// TC: O(nodes^2)
+// SC: O(nodes)
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int nodes = isConnected.length;
+        boolean[] visited = new boolean[nodes];
+        int provinces = 0;
+        
+        for(int node=0; node<nodes; node++){
+            if(!visited[node]){
+                provinces++;
+                dfs(node, isConnected, visited, nodes);
+            }
+        }
+        
+        return provinces;
+    }
+    
+    private void dfs(int node, int[][] isConnected, boolean[] visited, int nodes){
+        visited[node] = true;
+        int[] neighbors = isConnected[node];
+        
+        for(int neighbor=0; neighbor<nodes; neighbor++){
+            if(neighbors[neighbor]==1 && !visited[neighbor]) dfs(neighbor, isConnected, visited, nodes);
+        }
+    }
+}
